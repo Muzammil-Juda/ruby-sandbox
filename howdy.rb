b0a77d60cf9e481f8d9a61aeccbc575dd0d81488
Muzammil Juda <150502425+Muzammil-Juda@users.noreply.github.com>
2023-11-29 03:51:02 +0000

    initial commit

---
+++ howdy.rb
@@ -0,0 +1,15 @@
+require "active_support/all"
+require "./goodbye.rb"
+
+# my_string = "Howdy there!"
+
+#pp my_string
+
+x = 1
+#pp x.ordinalize
+
+pp "What's your name?"
+
+their_name = gets.chomp
+
+pp "Hello " + their_name +"!"
